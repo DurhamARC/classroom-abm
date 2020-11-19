e9b11e19f00d9af9951cf72df757143560ea3b67
Alison Clarke <alison.r.clarke@durham.ac.uk>
2020-11-19 12:46:06 +0000

    Added initial sbatch script and experiment file

---
+++ run_experiment.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Request resources:
+#SBATCH -n 4          # 4 CPU cores
+#SBATCH --mem=1G      # 1 GB RAM
+#SBATCH --time=0:15:0  # time (hours:minutes:seconds)
+
+# Run on the queue for serial ("sequential") work
+# (job will share node with other jobs)
+#SBATCH -p test.q
+
+# Commands to be run:
+../NetLogo\ 6.1.1/netlogo-headless.sh --model Classroom_mimic.nlogo \
+  --setup-file experiment.xml \
+  --experiment experiment \
+  --table classes_output/experiment.csv
