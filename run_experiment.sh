ac373beb9060e3790cd64cc4b8b2c0abd585efb5
Alison Clarke <alison.r.clarke@durham.ac.uk>
2020-12-03 15:35:06 +0000

    Attempt to run large set of parameters
    Broken down into multiple experiments and run using slurm arrays.

--- run_experiment.sh
+++ run_experiment.sh
@@ -3,17 +3,18 @@
 # Request resources:
 #SBATCH --nodes=1
 #SBATCH --mail-type=ALL
-#SBATCH --time=10:0:0  # time (hours:minutes:seconds)
+#SBATCH --time=24:0:0  # time (hours:minutes:seconds)
 
 # Run on the queue for serial ("sequential") work
 # (job will share node with other jobs)
-#SBATCH -p par6.q
+#SBATCH -p par7.q
+#SBATCH --array=1-6
 
 module load java/1.8.0
 
 # Commands to be run:
 ../NetLogo\ 6.1.1/netlogo-headless.sh --model Classroom_mimic.nlogo \
   --setup-file experiment-files/experiment4runs.xml \
-  --experiment experiment \
-  --table classes_output/experiment`date +%Y-%m-%d_%H%M%S`.csv \
-  --threads 16
+  --experiment experiment{$SLURM_ARRAY_TASK_ID} \
+  --table classes_output/experiment{$SLURM_ARRAY_TASK_ID}`date +%Y-%m-%d_%H%M%S`.csv \
+  --threads 24
