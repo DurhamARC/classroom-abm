03887dbb6b42a27e4e4dbb80e03e1320408aa723
Alison Clarke <alison.r.clarke@durham.ac.uk>
2021-03-26 16:14:09 +0000

    Added full model to see how explainer variables affect outcome.
    (Still need to add errors to summary table)

--- multilevel_analysis/multilevel_analysis.R
+++ multilevel_analysis/multilevel_analysis.R
@@ -19,4 +19,32 @@ null_summary_table <- data.frame(
              null_percentage_variance_class_level),
   row.names = c('Constant (mean)', 'Class variance', 'Pupil variance', '% variance at class level')
 )
+
+full_formula <- end_maths ~ 1 + (1 | class_id) + (1 | student_id) + start_maths + Inattentiveness + Ability + Deprivation
+full_model <- runMLwiN(full_formula, data=sorted_pupil_data)
+full_percentage_variance_class_level = full_model@RP[["RP2_var_Intercept"]] * 100 / 
+  (full_model@RP[["RP1_var_Intercept"]] + full_model@RP[["RP2_var_Intercept"]])
+full_summary_table <- data.frame(
+  'Actual'=c(full_model@FP[["FP_Intercept"]],
+             full_model@FP[["FP_start_maths"]],
+             full_model@FP[["FP_Inattentiveness"]],
+             full_model@FP[["FP_Ability"]],
+             full_model@FP[["FP_Deprivation"]],
+             full_model@RP[["RP2_var_Intercept"]], 
+             full_model@RP[["RP1_var_Intercept"]],
+             full_percentage_variance_class_level),
+  row.names = c('Constant (mean)',
+                'Start maths',
+                'Inattention',
+                'Ability',
+                'Deprivation',
+                'Class variance', 
+                'Pupil variance', 
+                '% variance at class level')
+)
+
+print("Null model:")
 print(null_summary_table)
+
+print("Full model:")
+print(full_summary_table)
