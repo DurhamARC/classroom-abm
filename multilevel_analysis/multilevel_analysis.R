9464ebfca20e3711082fac2429c96dc902bdde88
Alison Clarke <alison.r.clarke@durham.ac.uk>
2021-03-29 11:59:09 +0100

    Add std errors to summary tables

--- multilevel_analysis/multilevel_analysis.R
+++ multilevel_analysis/multilevel_analysis.R
@@ -7,44 +7,54 @@ pupil_data = numeric_pupil_data[complete.cases(numeric_pupil_data),]
 summary(pupil_data)
 sorted_pupil_data <- pupil_data[order(pupil_data$class_id),]
 
+# Create and summarise null model
 null_formula <- end_maths ~ 1 + (1 | class_id) + (1 | student_id)
 null_model <- runMLwiN(null_formula, data=sorted_pupil_data)
 
-null_percentage_variance_class_level = null_model@RP[["RP2_var_Intercept"]] * 100 / 
+null_model.percentage_variance_class_level = null_model@RP[["RP2_var_Intercept"]] * 100 / 
   (null_model@RP[["RP1_var_Intercept"]] + null_model@RP[["RP2_var_Intercept"]])
-null_summary_table <- data.frame(
-  'Actual'=c(null_model@FP[["FP_Intercept"]], 
-             null_model@RP[["RP2_var_Intercept"]], 
-             null_model@RP[["RP1_var_Intercept"]],
-             null_percentage_variance_class_level),
-  row.names = c('Constant (mean)', 'Class variance', 'Pupil variance', '% variance at class level')
-)
 
+null_model.summary_table <- data.frame(
+  coef(null_model),
+  sqrt(diag(vcov(null_model)))
+)
+names(null_model.summary_table) <- c("Actual", "Std Err")
+rownames(null_model.summary_table) = c('Constant (mean)', 'Class variance', 'Pupil variance')
+null_model.summary_table <- rbind(null_model.summary_table, 
+                                  "% of variance at class level" = c(
+                                    null_model.percentage_variance_class_level,
+                                    NA
+                                  ))
+
+# Create and summarise full model
 full_formula <- end_maths ~ 1 + (1 | class_id) + (1 | student_id) + start_maths + Inattentiveness + Ability + Deprivation
 full_model <- runMLwiN(full_formula, data=sorted_pupil_data)
-full_percentage_variance_class_level = full_model@RP[["RP2_var_Intercept"]] * 100 / 
+full_model.percentage_variance_class_level = full_model@RP[["RP2_var_Intercept"]] * 100 / 
   (full_model@RP[["RP1_var_Intercept"]] + full_model@RP[["RP2_var_Intercept"]])
-full_summary_table <- data.frame(
-  'Actual'=c(full_model@FP[["FP_Intercept"]],
-             full_model@FP[["FP_start_maths"]],
-             full_model@FP[["FP_Inattentiveness"]],
-             full_model@FP[["FP_Ability"]],
-             full_model@FP[["FP_Deprivation"]],
-             full_model@RP[["RP2_var_Intercept"]], 
-             full_model@RP[["RP1_var_Intercept"]],
-             full_percentage_variance_class_level),
-  row.names = c('Constant (mean)',
-                'Start maths',
-                'Inattention',
-                'Ability',
-                'Deprivation',
-                'Class variance', 
-                'Pupil variance', 
-                '% variance at class level')
+
+
+full_model.summary_table <- data.frame(
+  coef(full_model),
+  sqrt(diag(vcov(full_model)))
 )
+names(full_model.summary_table) <- c("Actual", "Std Err")
+rownames(full_model.summary_table) <- c(
+  'Constant (mean)',
+  'Start maths',
+  'Inattention',
+  'Ability',
+  'Deprivation',
+  'Class variance', 
+  'Pupil variance'
+)
+full_model.summary_table <- rbind(full_model.summary_table, 
+                                "% of variance at class level" = c(
+                                  full_model.percentage_variance_class_level,
+                                  NA
+                                ))
 
 print("Null model:")
-print(null_summary_table)
+print(round(null_model.summary_table,2))
 
 print("Full model:")
-print(full_summary_table)
+print(round(full_model.summary_table,2))
