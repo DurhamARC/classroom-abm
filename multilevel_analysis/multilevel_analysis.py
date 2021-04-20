10f43373d42632a6a7423a0f4ea32dabb816271e
Alison Clarke <alison.r.clarke@durham.ac.uk>
2021-04-20 09:12:09 +0100

    Ran 'black' over script

--- multilevel_analysis/multilevel_analysis.py
+++ multilevel_analysis/multilevel_analysis.py
@@ -7,23 +7,23 @@ import rpy2.robjects.packages as rpackages
 from rpy2.robjects import pandas2ri
 from rpy2.robjects.conversion import localconverter
 
-utils = rpackages.importr('utils')
+utils = rpackages.importr("utils")
 
 # Install R2MLWiN - TODO: see if we can cache this
 utils.chooseCRANmirror(ind=1)
-utils.install_packages('R2MLwiN')
-utils.install_packages('classroommlm', type = "source")
+utils.install_packages("R2MLwiN")
+utils.install_packages("classroommlm", type="source")
 
-base = rpackages.importr('base')
-classroom_mlm = rpackages.importr('classroommlm')
+base = rpackages.importr("base")
+classroom_mlm = rpackages.importr("classroommlm")
 
 # Import data from CSV to dataframe and clean
 csv_filename = os.path.join(
     os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
-    'classes_input/test_input.csv'
+    "classes_input/test_input.csv",
 )
 pupil_df = pd.read_csv(csv_filename)
-pupil_df.end_maths = pupil_df.end_maths.apply(pd.to_numeric, errors='coerce')
+pupil_df.end_maths = pupil_df.end_maths.apply(pd.to_numeric, errors="coerce")
 pupil_df = pupil_df.dropna()
 
 # Convert to R data frame
@@ -31,7 +31,7 @@ with localconverter(ro.default_converter + pandas2ri.converter):
     r_pupil_df = ro.conversion.py2rpy(pupil_df)
 
 # Null model
-r_null_model = classroom_mlm.null_model(r_pupil_df, '/opt/mln/mlnscript')
+r_null_model = classroom_mlm.null_model(r_pupil_df, "/opt/mln/mlnscript")
 
 with localconverter(ro.default_converter + pandas2ri.converter):
     null_model_df = ro.conversion.rpy2py(r_null_model)
@@ -39,7 +39,7 @@ with localconverter(ro.default_converter + pandas2ri.converter):
 print(null_model_df)
 
 # Full model
-r_full_model = classroom_mlm.full_model(r_pupil_df, '/opt/mln/mlnscript')
+r_full_model = classroom_mlm.full_model(r_pupil_df, "/opt/mln/mlnscript")
 
 with localconverter(ro.default_converter + pandas2ri.converter):
     full_model_df = ro.conversion.rpy2py(r_full_model)
