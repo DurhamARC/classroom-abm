a78ce8d7cf18c40b03354c4fd6d313e044976611
Alison Clarke <alison.r.clarke@durham.ac.uk>
2021-04-27 11:40:41 +0100

    Use mlwin script path from environment

--- multilevel_analysis/R/run_mlm/run_classroommlm.R
+++ multilevel_analysis/R/run_mlm/run_classroommlm.R
@@ -1,9 +1,15 @@
 library("classroommlm")
 library("R2MLwiN")
 
+
+mlnscript_path <- Sys.getenv("MLNSCRIPT_PATH")
+if (mlnscript_path == "") {
+  mlnscript_path = '/opt/mln/mlnscript'
+}
+
 pupil_data <- read.table("/Users/ksvf48/Documents/dev/classroom-abm/classes_input/test_input.csv", header=TRUE, sep=",")
-null_summary <- classroommlm::null_model(pupil_data, '/opt/mln/mlnscript')
+null_summary <- classroommlm::null_model(pupil_data, mlnscript_path)
 print(null_summary)
 
-full_summary <- classroommlm::full_model(pupil_data, '/opt/mln/mlnscript')
+full_summary <- classroommlm::full_model(pupil_data, mlnscript_path)
 print(null_summary)
