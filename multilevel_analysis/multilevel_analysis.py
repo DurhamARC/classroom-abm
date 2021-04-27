aa3d95c374d41f091c2065eb87333f8ac50a389d
Alison Clarke <alison.r.clarke@durham.ac.uk>
2021-04-27 12:00:52 +0100

    Try splitting out renv and main commands

--- multilevel_analysis/multilevel_analysis.py
+++ multilevel_analysis/multilevel_analysis.py
@@ -1,17 +1,23 @@
 import subprocess
 import sys
 
-completed = subprocess.run(
-    ["Rscript", "-e", 'renv::restore();source("run_classroommlm.R")'],
-    capture_output=True,
-    text=True,
-    cwd="./R/run_mlm",
-)
-print(completed.stdout)
 
-if completed.returncode:
-    print("Error running R script:")
-    print(completed.stderr)
-    sys.exit(1)
-else:
-    print("Success")
+def run_rscript(script):
+    completed = subprocess.run(
+        ["Rscript", "-e", script],
+        capture_output=True,
+        text=True,
+        cwd="./R/run_mlm",
+    )
+    print(completed.stdout)
+
+    if completed.returncode:
+        print("Error running R script:")
+        print(completed.stderr)
+        sys.exit(1)
+    else:
+        print("Success")
+
+
+run_rscript("renv::restore()")
+run_rscript('source("run_classroommlm.R")')
