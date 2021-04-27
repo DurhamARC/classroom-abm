a2cc3a4ebc3727edcf89f0f7adf04299dfe8c5fe
Alison Clarke <alison.r.clarke@durham.ac.uk>
2021-04-27 09:03:01 +0100

    Run renv:: restore in CI; check errors from python subprocess

--- multilevel_analysis/multilevel_analysis.py
+++ multilevel_analysis/multilevel_analysis.py
@@ -1,5 +1,16 @@
 import subprocess
+import sys
 
-subprocess.run(
-    ["Rscript", "-e", 'renv::load("R/run_mlm");source("R/run_mlm/run_classroommlm.R")']
+completed = subprocess.run(
+    ["Rscript", "-e", 'source("R/run_mlm/run_classroommlm.R")'],
+    capture_output=True,
+    text=True,
 )
+print(completed.stdout)
+
+if completed.returncode:
+    print("Error running R script:")
+    print(completed.stderr)
+    sys.exit(1)
+else:
+    print("Success")
