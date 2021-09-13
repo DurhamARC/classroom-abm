fc5b5258ffd6f33f549e3bfdea41376b46230ba6
Alison Clarke <alison.r.clarke@durham.ac.uk>
2021-09-13 11:40:32 +0100

    Don't include test_id in correlations.

--- hamilton/parameter_analysis/plot_correlations.py
+++ hamilton/parameter_analysis/plot_correlations.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
     file_path = os.path.join(dir, filename)
     df = pd.read_csv(file_path)
     y_vars = ["mean_squared_error"]
-    x_vars = list(df.columns.values)[:-1]
+    x_vars = list(df.columns.values)[1:-1]
     pp = sns.pairplot(data=df, kind="reg", y_vars=y_vars, x_vars=x_vars)
     pp.savefig(os.path.join(dir, "correlations.png"))
