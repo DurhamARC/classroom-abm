5f4537eeeb3953e70c9a6a4bb8e8b20c392a93f3
Alison Clarke <alison.r.clarke@durham.ac.uk>
2021-09-20 11:27:45 +0100

    Script to prepare parameter file for a new run
    Still needs to be added to docs

---
+++ hamilton/setup_new_run.sh
@@ -0,0 +1,19 @@
+cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null
+SCRIPT_DIR=`pwd`
+
+module purge
+module load miniconda2/4.1.11
+module load r/4.0.3
+source activate classroom_abm
+
+cd parameter_input
+python lhs_sampling.py
+export PARAMETER_FILE=`pwd`/lhs_params.csv
+echo "Will run with the following parameters: "
+column -s, -t $PARAMETER_FILE
+
+
+echo "If this looks OK, run the following commands to set off the batch jobs:"
+echo "cd $SCRIPT_DIR/reframe_parameterisation_infrastructure/reframe_tests"
+echo "export PARAMETER_FILE=`pwd`/lhs_params.csv"
+echo "./parameterisation.sh --with-big-dataset"
