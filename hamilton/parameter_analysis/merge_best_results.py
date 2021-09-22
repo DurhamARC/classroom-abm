0c83def6feb69e7b31a7d788b2e72d8233e400c8
Alison Clarke <alison.r.clarke@durham.ac.uk>
2021-09-22 16:17:05 +0100

    Allow parameterisation of MSE limit

--- hamilton/parameter_analysis/merge_best_results.py
+++ hamilton/parameter_analysis/merge_best_results.py
@@ -2,9 +2,19 @@ import os
 import pandas as pd
 import sys
 
+DEFAULT_MSE_LIMIT = 3
 
 if __name__ == "__main__":
     dataframes = []
+
+    if len(sys.argv) < 2:
+        print("Please provide the directory to search")
+        exit(1)
+
+    if len(sys.argv) > 2:
+        mse_limit = float(sys.argv[2])
+    else:
+        mse_limit = DEFAULT_MSE_LIMIT
 
     for d in os.listdir(sys.argv[1]):
         full_path = os.path.join(sys.argv[1], d)
@@ -12,7 +22,7 @@ if __name__ == "__main__":
             f = os.path.join(full_path, "lowest_to_highest_mses.csv")
             if os.path.isfile(f):
                 df = pd.read_csv(f, sep=",")
-                df = df.loc[(df["mean_squared_error"] < 3.5)]
+                df = df.loc[(df["mean_squared_error"] < mse_limit)]
                 df["directory"] = d
                 dataframes.append(df)
             else:
