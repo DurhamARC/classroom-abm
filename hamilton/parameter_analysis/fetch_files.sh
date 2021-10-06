6a85bbfc77143d4854fd58899bc7afca2aab0bb7
Alison Clarke <alison.r.clarke@durham.ac.uk>
2021-10-06 13:25:45 +0100

    Update fetch_files to work with automated repeats

--- hamilton/parameter_analysis/fetch_files.sh
+++ hamilton/parameter_analysis/fetch_files.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
-set -euo pipefail
 DATE_TO_FETCH=${DATE_TO_FETCH:-`date +"%Y-%m-%d"`}
+START_TIME='095825'
+END_TIME='105048'
 TIME_TO_FETCH=${TIME_TO_FETCH:-"*"}
+
+if [ -n "$START_TIME" ] && [ -n "$END_TIME" ]; then
+  TIME_TO_FETCH="{$START_TIME..$END_TIME}"
+fi
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 pushd $SCRIPT_DIR/../../parameterisation_results > /dev/null
@@ -12,16 +17,24 @@ LAST_DIR=${LAST_DIR:-${DIR_PREFIX}0}
 n=$((1+${LAST_DIR##$DIR_PREFIX}))
 NEW_DIR=$DIR_PREFIX$(printf "%03d" $n)
 mkdir $NEW_DIR
-
 pushd $NEW_DIR > /dev/null
-scp $USER@hamilton.dur.ac.uk:classroom-abm/hamilton/mse_results_from_reframe/mse_output_${DATE_TO_FETCH}_${TIME_TO_FETCH}.zip .
-unzip mse_output_${DATE_TO_FETCH}_${TIME_TO_FETCH}.zip
 
-DATA_DIR=`pwd`
-MSE_OUTPUT_FILE=`ls mse_output_${DATE_TO_FETCH}_${TIME_TO_FETCH}.csv`
+echo "Fetching file(s): /ddn/data/${USER}/classroom_abm_parameterisation_results/${DATE_TO_FETCH}_${TIME_TO_FETCH}.zip"
+scp -r ${USER}@hamilton.dur.ac.uk:/ddn/data/${USER}/classroom_abm_parameterisation_results/${DATE_TO_FETCH}_${TIME_TO_FETCH}.zip .
 
-python $SCRIPT_DIR/../parameter_analysis/merge_repeats.py $DATA_DIR/$MSE_OUTPUT_FILE
-python $SCRIPT_DIR/plot_correlations.py $DATA_DIR "lowest_to_highest_mses.csv"
+pwd
+
+for f in *; do
+  echo $f
+  subdir=${f%%.*}
+  mkdir $subdir
+  pushd $subdir > /dev/null
+  unzip ../${f}
+  popd
+done
+
+python $SCRIPT_DIR/merge_best_results.py `pwd`
+python $SCRIPT_DIR/plot_correlations.py `pwd` "best_mses.csv"
 
 popd > /dev/null
 popd > /dev/null
